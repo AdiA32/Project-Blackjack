a025317500eace3808a2a2fdb0c6d6f2710dddc8
Aditya <34532396+AdiA32@users.noreply.github.com>
2022-06-01 20:00:04 -0700

    deck implimentation

--- deck.py
+++ deck.py
@@ -19,6 +19,19 @@ class Deck:
     >>> deck.deal_hand(hand)
     >>> deck.get_cards()[0]
     (Q, clubs)
+
+    >>> d_hand = DealerHand()
+    >>> deck.deal_hand(d_hand)
+    >>> deck.deal_hand(d_hand)
+    >>> print(d_hand)
+    ____
+    |Q  |
+    | ♣ |
+    |__Q|
+    ____
+    |?  |
+    | ? |
+    |__?|
     """
 
     # Class Attribute(s)
@@ -27,7 +40,9 @@ class Deck:
         """
         Creates a Deck instance containing cards sorted in ascending order.
         """
-        ...
+        rank_list = list(range(2, 11)) + ["J", "Q", "K", "A"]
+        suit_list = ["clubs", "diamonds", "hearts", "spades"]
+        self.cards = [Card(rank, suit) for rank in rank_list for suit in suit_list]
 
     def shuffle(self, **shuffle_and_count):
         """Shuffles the deck using a variety of different shuffles.
@@ -37,13 +52,24 @@ class Deck:
             shuffle type and the number of times the shuffled
             should be called.
         """
-        ...
+        assert all(
+            [
+                isinstance(value, int) and value >= 0 and (key == "modified_overhand" or key == "mongean")
+                for key, value in shuffle_and_count.items()
+            ]
+        )
+        overhand_count = shuffle_and_count["modified_overhand"]
+        mongean_count = shuffle_and_count["mongean"]
+        self.cards = Shuffle.modified_overhand(self.cards, overhand_count)
+        for i in range(mongean_count):
+            self.cards = Shuffle.mongean(self.cards)
 
     def deal_hand(self, hand):
         """
         Takes the first card from the deck and adds it to `hand`.
         """
-        ...
+        assert isinstance(hand, PlayerHand)
+        hand.add_card(self.cards.pop(0))
 
     def get_cards(self):
-        ...
+        return self.cards
